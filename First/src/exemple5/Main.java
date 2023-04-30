aa5cbf0e81dc661c6a68d781972eab2dce0815fc
Richard Agarici <laurentiu.agarici@gmail.com>
2023-04-30 16:18:50 +0200

    adding today's work

---
+++ First/src/exemple5/Main.java
@@ -0,0 +1,27 @@
+package exemple5;
+
+public class Main {
+    public static void main(String[] args) {
+        String str =new String("netbeans");
+        System.out.println(str.length());
+        int lent = str.length();
+
+        String str2=str.toUpperCase();
+        System.out.println(str);
+        System.out.println(str2);
+
+        str=str.toUpperCase();
+        System.out.println(str);
+        System.out.println(str2);
+
+        str="    neatbeans    ";
+        str2=str.trim();
+        System.out.println(str);
+        System.out.println(str2);
+
+        str=str2.substring(4);
+//        str=str2.substring(4,4);
+        System.out.println(str);
+        System.out.println(str.replace('e','2'));
+    }
+}
