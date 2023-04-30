17eddb085b2515f314db0284d8f6701fea433f44
Richard Agarici <laurentiu.agarici@gmail.com>
2023-04-30 10:11:17 +0200

    updateing notes

--- First/src/exemple1/Main.java
+++ First/src/exemple1/Main.java
@@ -1,5 +1,13 @@
 package exemple1;
 
-public class main {
+public class Main {
+    public static void main(String[] args) {
 
+        int a=10, b=20;
+        float c =12.55f;
+        char d='A';
+        String str="Hello";
+        System.out.println(str);
+        System.out.println(str+(a+b));
+    }
 }
