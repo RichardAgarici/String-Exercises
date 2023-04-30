96b99bdc0a5c2ca9c2cf4a238fad423323ce0b5f
Richard Agarici <laurentiu.agarici@gmail.com>
2023-04-30 21:47:49 +0200

    update a few more exemples

---
+++ First/src/exemple6/Main.java
@@ -0,0 +1,24 @@
+package exemple6;
+
+public class Main {
+    public static void main(String[] args) {
+        String str1="Mr. Shahrukh Khan";
+        System.out.println(str1.startsWith("Mr."));
+        System.out.println(str1.startsWith("mr."));
+        System.out.println(str1.startsWith("Shah"));
+        System.out.println(str1.startsWith("Shah",4));
+        System.out.println(str1.endsWith("khan"));
+        System.out.println(str1.charAt(4));
+        for(int i=0;i<str1.length();i++){
+            System.out.print("\s"+str1.charAt(i));
+        }
+
+        String str2="www.udemy.co.in";
+        System.out.println("\n"+str2.indexOf('.'));
+        System.out.println(str2.indexOf("udemy"));
+        System.out.println(str2.indexOf('.',4));
+        System.out.println(str2.lastIndexOf('.'));
+
+
+    }
+}
---
+++ First/src/exemple7/Main.java
@@ -0,0 +1,25 @@
+package exemple7;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str1="JAVA";
+        String str2="java";
+        String str3="python";
+        String str4="python";
+
+        str3.equals(str4);
+        str1.equals(str2);
+
+        System.out.println(str1.equalsIgnoreCase(str2));
+        System.out.println(str2.compareTo(str3));
+        System.out.println(str1.compareTo(str3));
+        System.out.println(str3.compareTo(str2));
+
+
+
+
+
+
+    }
+}
