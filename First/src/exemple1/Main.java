33d2ad274fbc19d68f0bed2c09c191e5973312df
Richard Agarici <laurentiu.agarici@gmail.com>
2023-04-30 11:04:01 +0200

    update about regex index

--- First/src/exemple1/Main.java
+++ First/src/exemple1/Main.java
@@ -4,10 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         int a=10, b=20;
-        float c =12.55f;
-        char d='A';
-        String str="Hello";
-        System.out.println(str);
-        System.out.println(str+(a+b));
+        System.out.println("The sum of "+a+" and "+b+" is "+(a+b));
     }
 }
---
+++ First/src/exemple2/Main.java
@@ -0,0 +1,18 @@
+package exemple2;
+
+public class Main {
+    public static void main(String[] args) {
+        int x=10;
+        float y=12.55f;
+        char z ='A';
+        String str="Java Program";
+        System.out.printf("Hello");
+
+        System.out.printf("Hello %d %f %c World \n",x,y,z);
+        System.out.printf("Hello %e \n",y);
+        System.out.printf("Hello %s", str);
+
+        System.out.printf("\n%3$s %2$f %1$d", x,y,str);
+
+    }
+}
