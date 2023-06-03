a7d3e512ea778586f5cc9b9f7e0203161abfcd8a
Richard Agarici <laurentiu.agarici@gmail.com>
2023-06-03 15:33:01 +0200

    refreshing the newly added changes

--- First/src/challenges/challenge1/Main.java
+++ First/src/challenges/challenge1/Main.java
@@ -1,7 +1,12 @@
 package challenges.challenge1;
-
+//Find if the email id is on gmail
+//Find username and domain name form email
 public class Main {
     public static void main(String[] args) {
+        String str ="programmer@gmail.com" ;
 
+    }
+    public static String findIfGmail(String s){
+        if(s.matches("%"))
     }
 }
